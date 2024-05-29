f77e2ea1c9c3a4b429e0e86253ebf2759554088d
Mickaelio <94882142+tsiheje@users.noreply.github.com>
2024-05-29 10:47:13 +0300

    responsive navbar

--- src/components/BarNav.jsx
+++ src/components/BarNav.jsx
@@ -1,42 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import Github from "../asset/github.png";
-import facebook from "../asset/facebook.png"
-import linkedin from "../asset/linkedin.png";
+import facebook from "../asset/facebook (2).png"
+import linkedin from "../asset/linkedin (2).png";
+import Menu from "../asset/images/Menu.png";
 
 const BarNav = () => {
+    const [isOpen, setIsOpen] = useState(false); // State to track menu visibility
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
     return(
         <div className="navbar">
             <div className="logo">
                 Mickaelio<span>.</span>
             </div>
-            <div className="lien">
-                <a href="#home">
-                    Home
-                </a>
-                <a href="#about">
-                    About
-                </a>
-                <a href="#skills">
-                    Skills
-                </a>
-                <a href="#projects">
-                    Projects
-                </a>
-                <a href="#contact">
-                    Contact
-                </a>
+            <div className={`navbar-lien ${isOpen ? "active" : ""}`}>
+                <div className="lien">
+                    <a href="#home">
+                        Home
+                    </a>
+                    <a href="#about">
+                        About
+                    </a>
+                    <a href="#skills">
+                        Skills
+                    </a>
+                    <a href="#projects">
+                        Projects
+                    </a>
+                    <a href="#contact">
+                        Contact
+                    </a>
+                </div>
+                <div className="sociaux">
+                    <a href="https://www.facebook.com/Rasolofoniaina.Mickaelio/" target="_blank">
+                        <img src={facebook} alt="" width={40}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/rasolofoniaina-tsiheje-marie-mickaelio3/" target="_blank">
+                        <img src={linkedin} alt="" width={40}/>
+                    </a>
+                    <a href="https://github.com/tsiheje" target="_blank">
+                        <img src={Github} alt="" />
+                    </a>
+                </div>
             </div>
-            <div className="sociaux">
-                <a href="https://www.facebook.com/Rasolofoniaina.Mickaelio/" target="_blank">
-                    <img src={facebook} alt="" />
-                </a>
-                <a href="https://www.linkedin.com/in/rasolofoniaina-tsiheje-marie-mickaelio3/" target="_blank">
-                    <img src={linkedin} alt="" />
-                </a>
-                <a href="https://github.com/tsiheje" target="_blank">
-                    <img src={Github} alt="" />
-                </a>
+            <div className="menu">
+                <img src={Menu} alt="" width={40} onClick={toggleMenu}/>
             </div>
         </div>
     )
